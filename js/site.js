619673f1e7fd8d6b7d08bec0098043cadc918a05
omgitsfrancis <omgitsfrancis@gmail.com>
2018-03-01 13:33:36 -0800

    Add templates for all pages

--- js/site.js
+++ js/site.js
@@ -1,14 +1,43 @@
-var test = document.querySelector(".title-page");
+var navAbout = document.querySelector("#nav-about");
+var navProjects = document.querySelector("#nav-projects");
+var navContact = document.querySelector("#nav-contact");
 
-var about = document.querySelector('.about');
+var aboutPage = document.querySelector('.about');
+var projectsPage = document.querySelector('.projects');
+var contactPage = document.querySelector('.contact');
+
 var wrapper = document.querySelector('.wrapper');
 
-test.addEventListener('click', ()=>{
+navAbout.onclick =  ()=>{
     wrapper.classList.add('bringToView');
+    aboutPage.classList.add('active');
     console.log('test worked');
+};
+navProjects.onclick = ()=>{
+    wrapper.classList.add('bringToView');
+    projectsPage.classList.add('active');
+};
+navContact.onclick = function() {
+    wrapper.classList.add('bringToView');
+    contactPage.classList.add('active');
+};
+
+
+
+/* click wrapper to exit */
+wrapper.addEventListener('click', ()=>{
+
+    //aboutPage.classList.remove('active');
+    //projectsPage.classList.remove('active');
+    wrapper.classList.remove('bringToView');
+    removeActive();
 });
 
-about.addEventListener('click', ()=>{
-    wrapper.classList.remove('bringToView');
-    console.log('hide worked');
-});
+
+
+function removeActive(){
+    document.querySelectorAll('.active').forEach((value)=>{
+        value.classList.remove('active');
+    });
+    
+}
